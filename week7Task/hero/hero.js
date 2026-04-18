2c9cfacfc64efd71ac56412ab5ac9a96a901f3d3
Muhammad Arsalan <m.arsalan21686@gmail.com>
2026-04-18 15:31:08 +0500

    Updated week7Task

--- week7Task/hero/hero.js
+++ week7Task/hero/hero.js
@@ -1,41 +1,46 @@
+function showFunctionList() {
+    document.getElementById('main-trigger').style.display = 'none';
+    document.getElementById('function-list').style.display = 'grid';
+}
+
 const goToDetails = (functionName) => {
     if (functionName === 'alert') {
-        window.location.href = "..//alert/alert.html";
+        window.location.href = "./alert/alert.html";
     }
     if (functionName === 'prompt') {
-        window.location.href = "..//prompt/prompt.html";
+        window.location.href = "./prompt/prompt.html";
     }
     if (functionName === 'Math.sqrt') {
-        window.location.href = "..//mathSqrt/mathSqrt.html";
+        window.location.href = "./mathSqrt/mathSqrt.html";
     }
     if (functionName === 'toLowerCase') {
-        window.location.href = "..//toLowerCase/toLowerCase.html";
+        window.location.href = "./toLowerCase/toLowerCase.html";
     }   
     if (functionName === 'toUpperCase') {
-        window.location.href = "..//toUpperCase/toUpperCase.html";
+        window.location.href = "./toUpperCase/toUpperCase.html";
     }
     if (functionName === 'ArrowFunctions') {
-        window.location.href = "..//Arrow Functions/ArrowFunctions.html";
+        window.location.href = "./Arrow Functions/ArrowFunctions.html";
     }
     if (functionName === 'clearInterval') {
-        window.location.href = "..//clearInterval/clearInterval.html";
+        window.location.href = "./clearInterval/clearInterval.html";
     }
     if (functionName === 'Destructing') {
-        window.location.href = "..//Destructing/Destructing.html";
+        window.location.href = "./Destructing/Destructing.html";
     }
     if (functionName === 'RestOperator') {
-        window.location.href = "..//Rest Operator/RestOperator.html";
+        window.location.href = "./Rest Operator/RestOperator.html";
     }
     if (functionName === 'setInterval') {
-        window.location.href = "..//setInterval/setInterval.html";
+        window.location.href = "./setInterval/setInterval.html";
     }
     if (functionName === 'setTimeout') {
-        window.location.href = "..//setTimeOut/setTimeOut.html";
+        window.location.href = "./setTimeOut/setTimeOut.html";
     }
     if (functionName === 'SpreadOperator') {
-        window.location.href = "..//Spread Operator/SpreadOperator.html";
+        window.location.href = "./Spread Operator/SpreadOperator.html";
     }
     if (functionName === 'TemplateLiterals') {
-        window.location.href = "..//Template Literals/TemplateLiterals.html";
+        window.location.href = "./Template Literals/TemplateLiterals.html";
     }
 }
--- week7Task/script.js
+++ week7Task/script.js
@@ -1,36 +1,30 @@
-fetch("../header/header.html")
+fetch("./header/header.html")
 .then(res => res.text())
 .then(data => {
     document.getElementById("header").innerHTML = data;
 });
+
 const link = document.createElement("link");
     link.rel = "stylesheet";
-    link.href = "../header/header.css";
+    link.href = "./header/header.css";
     document.head.appendChild(link);
 
-
-function showFunctionList() {
-
-    document.getElementById('main-trigger').style.display = 'none';
-    document.getElementById('function-list').style.display = 'flex';
-}
-
-fetch("../hero/hero.html")
+fetch("./hero/hero.html")
 .then(res => res.text())
 .then(data => {
     document.getElementById("hero").innerHTML = data;
 });
+
 const linkHeroCss = document.createElement("link");
 linkHeroCss.rel = "stylesheet";
-linkHeroCss.href = "../hero/hero.css";
+linkHeroCss.href = "./hero/hero.css";
 document.head.appendChild(linkHeroCss);
 
 const script = document.createElement("script");
-    script.src = "../hero/hero.js";
+    script.src = "./hero/hero.js";
     document.body.appendChild(script);
 
-
-fetch("../footer/footer.html")
+fetch("./footer/footer.html")
 .then(res => res.text())
 .then(data => {
     document.getElementById("footer").innerHTML = data;
@@ -38,6 +32,5 @@ fetch("../footer/footer.html")
 
 const linkFooterCss = document.createElement("link");
 linkFooterCss.rel = "stylesheet";
-linkFooterCss.href = "../footer/footer.css";
+linkFooterCss.href = "./footer/footer.css";
 document.head.appendChild(linkFooterCss);
-
